e033d3aa5d86be40a40f472ea1c3122da6d12f35
Kristian Jakobsen <kristian@nymann.dev>
2023-08-23 20:07:04 +0200

    Add solution for project 1

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css'],
+})
+export class WarningAlertComponent {
+  @Input() text: string = 'Default Warning!';
+}
